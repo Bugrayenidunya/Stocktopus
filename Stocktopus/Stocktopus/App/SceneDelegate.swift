73ae3ff42d4290294db21b9d07288343a47934fd
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2023-10-05 22:12:06 +0300

    Make the app not use a storyboard

---
+++ Stocktopus/Stocktopus/App/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  Stocktopus
+//
+//  Created by Enes Buğra Yenidünya on 5.10.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    // MARK: Properties
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let viewController = ViewController()
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+}
