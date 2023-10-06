b494be7c7f372e13fe5bcf5b9cad8dffaea8312a
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2023-10-06 18:56:55 +0300

    Add constants

---
+++ Stocktopus/Stocktopus/Utilities/Constants/API.swift
@@ -0,0 +1,14 @@
+//
+//  API.swift
+//  Stocktopus
+//
+//  Created by Enes Buğra Yenidünya on 6.10.2023.
+//
+
+import Foundation
+
+extension Constant {
+    class API {
+        static let baseUrl = "https://interview-api.dev-getmidas.com/"
+    }
+}
---
+++ Stocktopus/Stocktopus/Utilities/Constants/Constant.swift
@@ -0,0 +1,10 @@
+//
+//  Constant.swift
+//  Stocktopus
+//
+//  Created by Enes Buğra Yenidünya on 6.10.2023.
+//
+
+import Foundation
+
+class Constant { }
