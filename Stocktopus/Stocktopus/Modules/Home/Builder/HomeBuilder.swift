9ec0a3cacafced655e7a10d0fa168d74badd3bb5
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2023-10-07 12:48:58 +0300

    Update HomeBuilder as required

--- Stocktopus/Stocktopus/Modules/Home/Builder/HomeBuilder.swift
+++ Stocktopus/Stocktopus/Modules/Home/Builder/HomeBuilder.swift
@@ -9,11 +9,11 @@ import Foundation
 
 final class HomeBuilder {
     static func build() -> HomeController {
+        let networManager = NetworkManager.shared
         let router = HomeRouter()
-        
-        let viewModel = HomeViewModel(router: router)
-        
+        let viewModel = HomeViewModel(router: router, networkManager: networManager)
         let controller = HomeController(viewModel: viewModel)
+        
         viewModel.output = controller
         
         return controller
